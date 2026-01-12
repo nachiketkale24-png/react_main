161886fa7b6173e1b64e3cab584923f3305d0a58
NachiketKale <nachiket.kale24@spit.ac.in>
2026-01-12 23:59:59 +0530

    fix: correct main.jsx JSX usage

--- 01basicvite/src/main.jsx
+++ 01basicvite/src/main.jsx
@@ -1,8 +1,35 @@
 
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
+const reactElement = {
+    type: 'a',
+    props: {
+        href: 'https://www.google.com',
+        target: '_blank'
+    },
+    children: 'click me to visit google'
+}
+
+const anotherElement = (
+    <a href="https://www.google.com" target="_blank">visit google</a>
+)
+
+function MyApp() {
+    return (
+        <div>
+            <h1>Custom React App</h1>
+        </div>
+    )
+}
+
+const areactElement = React.createElement(
+    'a',
+    {href: 'https://www.google.com', target: '_blank'},
+    'click me to visit google'
+)
+
+ReactDOM.createRoot(document.getElementById('root')).render(
     <App />
 )
